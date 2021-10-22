12d99364b66585aa0bc58179ad776b77cda614c3
Ryan Li <rl3294@nyu.edu>
2021-10-22 00:03:09 -0400

    Finish Turtle Module

---
+++ ch09 - Graphics and Animation with the Turtle Module/create_lines.py
@@ -0,0 +1,30 @@
+import turtle as t
+
+t.Screen()
+t.setup(600,500,100,200)
+t.bgcolor('lightgreen')
+t.title('Python Turtle Graphics')
+t.pensize(6)
+t.goto(200,100)
+t.up()
+t.pencolor('blue')
+for i in range(8):
+    t.goto(-200+50*i,-150)
+    t.down()
+    t.goto(-200+50*i+30,-150)
+    t.up()
+
+for i in range(8):
+    t.goto(-200+50*i, -50)
+    t.down()
+    t.goto(-200+50*i+30, -50)
+    t.up()
+
+t.hideturtle()
+t.done()
+
+try:
+    t.bye()
+except t.Terminator:
+    print('exit turtle')
+
